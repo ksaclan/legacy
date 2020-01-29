46bbce52233152c2cb179f7249346da8c53b31a1
Tyler Hoban <admin@tylerhoban.com>
2020-01-29 13:13:04 -0500

    Update inspri.sh

--- inspri.sh
+++ inspri.sh
@@ -1,10 +1,10 @@
-sudo tee /etc/yum.repos.d/mongodb-org-4.0.repo << EOF
-[mongodb-org-4.0]
+sudo tee /etc/yum.repos.d/mongodb-org-4.2.repo << EOF
+[mongodb-org-4.2]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/7/mongodb-org/4.0/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/7/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc
 EOF
 
 sudo tee /etc/yum.repos.d/pritunl.repo << EOF
